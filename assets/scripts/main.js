395eaf94ce758de94374ceb6a4185170f132a878
Yoomin <y5oh@ucsd.edu>
2021-10-31 16:58:57 -0700

    Fixing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,9 +6,9 @@ const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
   'https://introweb.tech/assets/json/birthdayCake.json',
   'https://introweb.tech/assets/json/chocolateChip.json',
-  'assets/recipes/food3.json',
-  'assets/recipes/food4.json',
-  'assets/recipes/food2.json',
+  '/assets/recipes/food3.json',
+  '/assets/recipes/food4.json',
+  '/assets/recipes/food2.json',
 
 ];
 
